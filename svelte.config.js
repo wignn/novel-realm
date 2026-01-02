43b3a1dd080ab3211dda3c169e246d3d56668711
wignn <tigfiragnafatur1933@gmail.com>
2026-01-02 20:51:27 +0700

    chore: Add nodejs22.x runtime configuration to SvelteKit adapter.

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,11 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			runtime: 'nodejs22.x'
+		})
+	}
 };
 
 export default config;
